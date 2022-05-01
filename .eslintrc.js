26908ee7755a6d04c90ff73cdd69b13b02e1b62c
linh <571187984@qq.com>
2022-05-01 17:23:45 +0800

    this is qiankun sub app

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,16 @@ module.exports = {
   env: {
     node: true
   },
+  globals: {
+    __webpack_public_path__: true
+  },
   extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,34 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+const { name } = require('./package')
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+const port = 8888
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    // host: '0.0.0.0',
+    hot: true,
+    port,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  // 自定义webpack配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    },
+    output: {
+      // 把子应用打包成 umd 库格式
+      library: `${name}-[name]`,
+      libraryTarget: 'umd',
+      globalObject: 'window',
+      chunkLoadingGlobal: `webpackJsonp_${name}`
+    }
+  }
 })
